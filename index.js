3f51843eca502bf3a1964da9b9e3364e0bcc4811
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-08-29 07:38:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ rejectionEmitter.on("rejectionHandled", (error, promise) => {
     fs.writeFileSync("./data.json", JSON.stringify(error), "utf8");
 })
 var GlideRecord = require('servicenow-rest').gliderecord;
-var gr = new GlideRecord('dev43073', 'x_58872_needit_needit', 'admin', 'DEUCD78YCgkJ');
-gr.setReturnFields('number,short_description');
-gr.addEncodedQuery('active=true');
-gr.setLimit(10);
-gr.query().then(function (result) { //returns promise 
-    console.log(result);
-})
-
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
@@ -49,9 +41,7 @@ intents.matches('Default Welcome Intent', [
 
 intents.matches('Add user', [
     function (session, args) {
-        console.log("Add user")
-        console.log("Args : " + JSON.stringify(args));
-        var responseString = "Sure !"
+        var gr = new GlideRecord('dev43073', 'sys_user', 'admin', 'DEUCD78YCgkJ');
         session.send(responseString);
     }
 ]);
